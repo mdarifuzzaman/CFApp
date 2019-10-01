4904539cffb49a2200a2eaa220ba1e6a005e364b
Md Arifuzzaman <arif_uap@outlook.com>
2019-10-01 18:51:47 +1000

    add doc folder

---
+++ doc/precache-manifest.be728e69146a413c1db8694f4c7bfa8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7eb7e75bc925a50d872f1dee3f5bc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321bdb6643dbd15f8836",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "a3e020c561ed3b455e57",
+    "url": "/static/css/main.508cd231.chunk.css"
+  },
+  {
+    "revision": "321bdb6643dbd15f8836",
+    "url": "/static/js/2.f199d4dd.chunk.js"
+  },
+  {
+    "revision": "a3e020c561ed3b455e57",
+    "url": "/static/js/main.66506f26.chunk.js"
+  },
+  {
+    "revision": "88e02f17626951c131a7",
+    "url": "/static/js/runtime-main.ff08d67d.js"
+  }
+]);
