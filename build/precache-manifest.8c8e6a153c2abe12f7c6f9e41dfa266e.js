e0bf46f45b2bae58014a2b4ca7d83092120d40b6
mdarifuzzaman <arif_uap@outlook.com>
2019-10-01 16:48:12 +1000

    Add files via upload

---
+++ build/precache-manifest.8c8e6a153c2abe12f7c6f9e41dfa266e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59865066dcdf4c0d3a7e37fb8fd1533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321bdb6643dbd15f8836",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5a404c5004019ae5d86d",
+    "url": "/static/css/main.508cd231.chunk.css"
+  },
+  {
+    "revision": "321bdb6643dbd15f8836",
+    "url": "/static/js/2.f199d4dd.chunk.js"
+  },
+  {
+    "revision": "5a404c5004019ae5d86d",
+    "url": "/static/js/main.3d7979e7.chunk.js"
+  },
+  {
+    "revision": "88e02f17626951c131a7",
+    "url": "/static/js/runtime-main.ff08d67d.js"
+  }
+]);
