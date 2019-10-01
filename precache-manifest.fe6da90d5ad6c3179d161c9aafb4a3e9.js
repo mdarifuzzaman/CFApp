0c7d70a4c6736ed68f776f3bd09596eb9fe8a624
Md Arifuzzaman <arif_uap@outlook.com>
2019-10-01 20:03:38 +1000

    Updates

---
+++ precache-manifest.fe6da90d5ad6c3179d161c9aafb4a3e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a96348b84f47ffb0159ea2d311b737f",
+    "url": "/CFApp/index.html"
+  },
+  {
+    "revision": "321bdb6643dbd15f8836",
+    "url": "/CFApp/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "f88dfa55496ba947d5d7",
+    "url": "/CFApp/static/css/main.508cd231.chunk.css"
+  },
+  {
+    "revision": "321bdb6643dbd15f8836",
+    "url": "/CFApp/static/js/2.f199d4dd.chunk.js"
+  },
+  {
+    "revision": "f88dfa55496ba947d5d7",
+    "url": "/CFApp/static/js/main.038b5736.chunk.js"
+  },
+  {
+    "revision": "505c7d37c5b1ed37dedc",
+    "url": "/CFApp/static/js/runtime-main.f80eba64.js"
+  }
+]);
